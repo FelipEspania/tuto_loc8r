1dd33d1da363a61f89c281a8306654c2b65b503c
fespana <felipe.espana1988@gmail.com>
2016-06-01 20:31:37 -0400

    datos en controladores

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,11 +1,17 @@
 module.exports.homelist = function (req, res){
-	res.render('locations-list', { title: 'Home'});
-}
+	res.render('locations-list', {
+		title: 'Loc8r - find a place to work with wifi',
+		pageHeader:{
+			title: 'Loc8r',
+			strapline: 'Find places to work with wifi near you!'
+		}
+	});
+};
 
 module.exports.locationInfo = function(req, res){
 	res.render('location-info', { title: 'Location Info'});
-}
+};
 
 module.exports.addReview = function(req, res){
 	res.render('location-review-form', { title: 'Add Review'});
-}
+};
