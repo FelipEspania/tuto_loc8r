65915429213cdb92c438779d7822ad62004daad3
fespana <felipe.espana1988@gmail.com>
2016-06-02 23:06:43 -0400

    fin de diseño de la web

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -5,23 +5,24 @@ module.exports.homelist = function (req, res){
 			title: 'Loc8r',
 			strapline: 'Find places to work with wifi near you!'
 		},
+		sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.",
 		locations:[{
 			name: 'StartCups',
 			address: '125 High Street, Reading, RG6 1PS',
-			rating: '3',
-			facilities: ['Hot drinks, Food, Premium wifi'],
+			rating: 3,
+			facilities: ['Hot drinks', 'Food', 'Premium wifi'],
 			distance: '100m'
 		},{
 			name: 'Rpro',
 			address: 'Roman diaz, Providencia, 688',
-			rating: '4',
-			facilities: ['Coffee, Food, Free wifi'],
+			rating: 4,
+			facilities: ['Coffee', 'Food', 'Free wifi'],
 			distance: '200m'
 		},{
 			name: 'MeMe',
 			address: 'Conferencia, Santiago, 1380',
-			rating: '5',
-			facilities: ['Food, Free wifi'],
+			rating: 5,
+			facilities: ['Food', 'Free wifi'],
 			distance: '1000m'
 		}]
 	});
